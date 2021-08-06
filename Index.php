e5246407ca63c7d429834baf2ef4f1e304fe891d
dantelazz <69008060+dantelazz@users.noreply.github.com>
2021-08-06 16:40:06 -0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,1 +1,15 @@
-<?php require_once("main.html")?>
+
+<?php
+echo '<!DOCTYPE html>';
+echo '<html lang="en">';
+echo '<head>';
+echo '<meta charset="UTF-8">';
+echo '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
+echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+echo '<title>Document</title>';
+echo '</head>';
+echo '<body>';
+echo '<h1>Hola que tal</h1>';
+echo '</body>';
+echo '</html>';
+?>
